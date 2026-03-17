dff33afeea90246db557874d7a6d56822baea15a
Snehasish Parida <snehasish.parida016@gmail.com>
2026-03-17 10:48:57 +0530

    filter functionality in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,25 +3,28 @@ import { ProductContext } from "../context/ProdContext";
 import ProductCard from "../components/ProductCard";
 
 export default function Home() {
-
     const { products } = useContext(ProductContext);
 
     const [category, setCategory] = useState("all");
 
-    const categories = ["all", ...new Set(products.map(p => p.category))];
+    const categories = [
+        "all",
+        ...products
+            .map((p) => p.category)
+            .filter((cat, index, arr) => arr.indexOf(cat) === index),
+    ];
 
     const filteredProducts =
         category === "all"
             ? products
-            : products.filter(p => p.category === category);
+            : products.filter((p) => p.category === category);
 
     return (
         <div className="container my-4">
-
-
             <div className="d-flex justify-content-between align-items-center mb-4">
-
-                <h2 className="fst-italic text-secondary text-decoration-underline">Products</h2>
+                <h2 className="fst-italic text-secondary text-decoration-underline">
+                    Products
+                </h2>
 
                 <select
                     className="form-select w-auto"
@@ -34,21 +37,13 @@ export default function Home() {
                         </option>
                     ))}
                 </select>
-
             </div>
-
 
             <div className="row">
-
                 {filteredProducts.map((product) => (
-                    <ProductCard
-                        key={product.id}
-                        product={product}
-                    />
+                    <ProductCard key={product.id} product={product} />
                 ))}
-
             </div>
-
         </div>
     );
 }
